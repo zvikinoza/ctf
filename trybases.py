0512f0bd42bae942541db7d8a987ab457ab1f741
zvikinoza <zvikinoza@gmail.com>
2022-01-28 22:57:45 +0400

    based

---
+++ trybases.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+
+def b2s(string, base):
+	return ''.join([chr(int(s, base)) for s in string.split()])
+
+
+if len(sys.argv) < 2:
+	print('invalid args')
+	exit()
+
+string = ' '.join(sys.argv[1:])
+for base in [2, 4, 8, 16, 32]:
+	s = 'err'
+	try:
+		s = ''.join([chr(int(s, base)) for s in string.split()]);
+	except:
+		pass	
+	print(f'base={base} string={s}')
+
+try:
+	print(f"decoded={bytes.fromhex(string).decode('utf-8')}")
+except:
+	print('err from hex')
+
+
